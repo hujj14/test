468301fee154dee44daeddf5b1cc8a42d0b44485
Mihail <smyshmish@gmail.com>
2024-09-09 22:09:58 +0300

    first commit

---
+++ kalkolator.cpp
@@ -0,0 +1,47 @@
+﻿// kalkolator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	double a;
+    double b;
+	
+	 cout << "Specify the first number\n";
+	 cin >> a;
+	 cout << "Specify the second number\n";
+	 cin >> b;
+	 cout << "select the calculus selection\n";
+	 int choise;
+	 double result;
+
+	 if (choise == 1)
+		  result = a + b;
+	  int choise;
+	  double result;
+
+	 if (choise == 2)
+		  result = a - b;
+	  int choise;
+	 double result;
+
+	 if (choise == 3)
+		 result = a / b;
+	  int choise;
+	  double result;
+
+	 if (choise == 4)
+		  result = a * b;
+	  int choise;
+	  double result;
+  
+
+  
+  
+}
+
+
+
